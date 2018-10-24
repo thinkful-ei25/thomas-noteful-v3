18c36a88b98a2fa2f7857d3c804e213a332f2e6e
Thomas Chun <tchun2055@gmail.com>
2018-10-23 17:16:40 -0700

    updated notes endpoints

--- routes/notes.js
+++ routes/notes.js
@@ -4,6 +4,7 @@ const express = require('express');
 const Note = require('../models/note');
 const router = express.Router();
 
+
 /* ========== GET/READ ALL ITEMS ========== */
 router.get('/', (req, res, next) => {
   const { searchTerm } = req.query;
@@ -16,44 +17,98 @@ router.get('/', (req, res, next) => {
 
   Note
     .find(filter)
-    .sort({ updatedAt: 'desc' })
+    // .sort({ updatedAt: 'desc' })
     .then(results => {
       res.json(results);
     })
-    .catch(error => {
-      next(error);
-    });
+    .catch(err => next(err));
 });
+
 
 /* ========== GET/READ A SINGLE ITEM ========== */
 router.get('/:id', (req, res, next) => {
+  const id = req.params.id;
 
-  console.log('Get a Note');
-  res.json({ id: 1, title: 'Temp 1' });
-
+  Note
+    .findById(id)
+    .sort({ id: 'asc' })
+    .then(results => {
+      if (!results) {
+        const err = new Error('Not Found!');
+        err.status = 404;
+        return next(err);
+      }
+      if (results) { 
+        res.json(results);
+      } else {
+        next();
+      }
+    })
+    .catch(err => next(err));
 });
+
 
 /* ========== POST/CREATE AN ITEM ========== */
 router.post('/', (req, res, next) => {
+  const { title, content } = req.body;
 
-  console.log('Create a Note');
-  res.location('path/to/new/document').status(201).json({ id: 2, title: 'Temp 2' });
+  // if (!title) {
+  //   const err = new Error('Missing `title` in request body');
+  //   err.status = 400;
+  //   return next(err);
+  // }
 
+  const newNote = {
+    title,
+    content,
+  };
+
+  Note
+    .create(newNote)
+    .then(result => {
+      // res.json(result);
+      res.location(`${result.id}`).status(201).json(result);
+    })
+    .catch(err => next(err));
 });
+
 
 /* ========== PUT/UPDATE A SINGLE ITEM ========== */
 router.put('/:id', (req, res, next) => {
+  const id = req.params.id;
+  const updateNote = {};
+  const updateableFields = ['title', 'content'];
 
-  console.log('Update a Note');
-  res.json({ id: 1, title: 'Updated Temp 1' });
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`);
+    console.error(message);
+    // we return here to break out of this function
+    return res.status(400).json({message: message});
+  }
 
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      updateNote[field] = req.body[field];
+    }
+  });
+
+  Note
+    .findByIdAndUpdate(id, { $set: updateNote }, { new: true })
+    .then(result => {
+      res.json(result).status(204).end();
+    })
+    .catch(err => next(err));
 });
 
 /* ========== DELETE/REMOVE A SINGLE ITEM ========== */
 router.delete('/:id', (req, res, next) => {
-
-  console.log('Delete a Note');
-  res.status(204).end();
+  const id = req.params.id;
+  Note
+    .findByIdAndRemove(id)
+    .then(() => res.status(204).end())
+    .catch(err => next(err));
 });
 
 module.exports = router;
--- scratch/queries.js
+++ scratch/queries.js
@@ -35,98 +35,98 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
 
 
 
-// /********** FIND NOTE BY ID (Note.findById) ***********/
-// mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
-//   .then(() => {
-//     const noteId = '000000000000000000000002';
-//     return Note.findById(noteId);
-//   })
-//   .then(results => {
-//     console.log(results);
-//   })
-//   .then(() => {
-//     return mongoose.disconnect();
-//   })
-//   .catch(err => {
-//     console.error(`ERROR: ${err.message}`);
-//     console.error(err);
-//   });
+/********** FIND NOTE BY ID (Note.findById) ***********/
+mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+  .then(() => {
+    const noteId = '000000000000000000000002';
+    return Note.findById(noteId);
+  })
+  .then(results => {
+    console.log(results);
+  })
+  .then(() => {
+    return mongoose.disconnect();
+  })
+  .catch(err => {
+    console.error(`ERROR: ${err.message}`);
+    console.error(err);
+  });
 
 
 
-// /********** CREATE NEW NOTE (Note.create) ***********/
-// mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
-//   .then(() => {
-//     const newNote = {
-//       title: 'new title',
-//       content: 'new content'
-//     };
-//     return Note.create(newNote);
-//   })
-//   .then(results => {
-//     console.log(results);
-//   })
-//   .then(() => {
-//     return mongoose.disconnect();
-//   })
-//   .catch(err => {
-//     console.error(`ERROR: ${err.message}`);
-//     console.error(err);
-//   });
+/********** CREATE NEW NOTE (Note.create) ***********/
+mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+  .then(() => {
+    const newNote = {
+      title: 'new title',
+      content: 'new content'
+    };
+    return Note.create(newNote);
+  })
+  .then(results => {
+    console.log(results);
+  })
+  .then(() => {
+    return mongoose.disconnect();
+  })
+  .catch(err => {
+    console.error(`ERROR: ${err.message}`);
+    console.error(err);
+  });
 
 
 
-// /********** UPDATE NOTE BY ID (Note.findByIdAndUpdate) ***********/
-// mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
-//   .then(() => {
-//     const noteId = '000000000000000000000001';
-//     const updateNote = {
-//       title: 'Updated test title 1',
-//       content: 'Updated test content 1',
-//       // updatedAt: Date.now -- needed if line 13 in note.js didn't exist.
-//     };
-//     return Note.findByIdAndUpdate(noteId, updateNote, {new: true}); 
-//     // *NOTE -- {upsert: true} would also create if note doesn't exist.
-//     // {new: true} -> return the inserted or updated document.
-//   })
-//   .then(results => {
-//     console.log(results);
-//   })
-//   .then(() => {
-//     return mongoose.disconnect();
-//   })
-//   .catch(err => {
-//     console.error(`ERROR: ${err.message}`);
-//     console.error(err);
-//   });
+/********** UPDATE NOTE BY ID (Note.findByIdAndUpdate) ***********/
+mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+  .then(() => {
+    const noteId = '000000000000000000000001';
+    const updateNote = {
+      title: 'Updated test title 1',
+      content: 'Updated test content 1',
+      // updatedAt: Date.now -- needed if line 13 in note.js didn't exist.
+    };
+    return Note.findByIdAndUpdate(noteId, updateNote, {new: true}); 
+    // *NOTE -- {upsert: true} would also create if note doesn't exist.
+    // {new: true} -> return the inserted or updated document.
+  })
+  .then(results => {
+    console.log(results);
+  })
+  .then(() => {
+    return mongoose.disconnect();
+  })
+  .catch(err => {
+    console.error(`ERROR: ${err.message}`);
+    console.error(err);
+  });
 
 
 
-// /********** DELETE A NOTE (Note.findByIdAndRemove) ***********/
-// mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
-//   .then(() => {
-//     const noteId = '000000000000000000000007';
-//     return Note.findByIdAndRemove(noteId);
-//     // *NOTE - ^ removes note by id but returns the removed value
-//     // so below, results with be passed the removed value, thus console logging the note.
-//     // Run the commented out test code below to test the item was removed.
-//     // Should return a value of null if remove was successful.
-//   })
-//   .then(results => {
-//     console.log('delete', results);
-//   })
-//   /****** TEST TO CHECK FOR REMOVAL ******/
-//   // .then(() => {
-//   //   const noteId = '000000000000000000000007';
-//   //   return Note.findById(noteId);
-//   // })
-//   // .then(results => {
-//   //   console.log('find by id', results);
-//   // })
-//   .then(() => {
-//     return mongoose.disconnect();
-//   })
-//   .catch(err => {
-//     console.error(`ERROR: ${err.message}`);
-//     console.error(err);
-//   });
+/********** DELETE A NOTE (Note.findByIdAndRemove) ***********/
+mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+  .then(() => {
+    const noteId = '000000000000000000000007';
+    return Note.findByIdAndRemove(noteId);
+    // *NOTE - ^ removes note by id but returns the removed value
+    // so below, results with be passed the removed value, thus console logging the note.
+    // Run the commented out test code below to test the item was removed.
+    // Should return a value of null if remove was successful.
+  })
+  .then(results => {
+    console.log('delete', results);
+  })
+  /****** TEST TO CHECK FOR REMOVAL ******/
+  // .then(() => {
+  //   const noteId = '000000000000000000000007';
+  //   return Note.findById(noteId);
+  // })
+  // .then(results => {
+  //   console.log('find by id', results);
+  // })
+  .then(() => {
+    return mongoose.disconnect();
+  })
+  .catch(err => {
+    console.error(`ERROR: ${err.message}`);
+    console.error(err);
+  });
